3308c03b43493bd090ead39adad519404eda44ed
BlzFans <BlzFans@hotmail.com>
2011-10-16 13:03:01 +0000

    cairo version changed to 1.8.10

--- WebKitLibraries/win/include/cairo/cairo.h
+++ WebKitLibraries/win/include/cairo/cairo.h
@@ -50,7 +50,13 @@
 # define CAIRO_END_DECLS
 #endif
 
-#define cairo_public
+#ifndef cairo_public
+# if defined (_MSC_VER) && ! defined (CAIRO_WIN32_STATIC_BUILD)
+#  define cairo_public __declspec(dllimport)
+# else
+#  define cairo_public
+# endif
+#endif
 
 CAIRO_BEGIN_DECLS
 
